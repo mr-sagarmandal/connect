0ded3addece6ae96705b91a24e92bdd0c251bfbd
andrewge8622 <andrewge8622@gmail.com>
2017-07-15 07:35:48 -0700

    Add files via upload

--- parse.ps1
+++ parse.ps1
@@ -1,11 +1,7 @@
 cd ResumeTransducer
 $env:GATE_HOME="..\GATEFiles"
-java -cp '.\\bin\\*;..\\GATEFiles\\lib\\*;..\\GATEFILES\\bin\\gate.jar;.\\lib\\*' code4goal.antony.resumeparser.ResumeParserProgram .\\UnitTests\\Andrew_Ge_Resume.pdf parsedresume.txt
+java -cp '.\\bin\\*;..\\GATEFiles\\lib\\*;..\\GATEFILES\\bin\\gate.jar;.\\lib\\*' code4goal.antony.resumeparser.ResumeParserProgram .\\UnitTests\\AntonyDeepakThomas.pdf parsedresume.json
 cd ResumeProcessing/src
-Get-Content ../../parsedresume.txt
-Get-Content ../../parsedresume.txt | java RegexMatches > parsedskills.txt
-Get-Content parsedskills.txt
-java Parser parsedskills.txt
-rm parsedskills.txt
-rm ../../parsedresume.txt
+java Parser ../../parsedresume.json
+rm ../../parsedresume.json
 cd ../../..
