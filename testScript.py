cc1b887c3b09ac24dca5fe69dc031691102e3646
Ahmad Javadi <sjavadi@cs.stonybrook.edu>
2017-07-15 08:10:52 +0000

    added new code

---
+++ testScript.py
@@ -0,0 +1,13 @@
+import happybase
+
+connection = happybase.Connection('localhost')
+connection.open()
+
+table = connection.table('resume')
+
+for key, data in table.scan():
+    print key
+    row = table.row(key, columns = [b'skill'])
+    for keys in row:
+        print row[keys]
+    
