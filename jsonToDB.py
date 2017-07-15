5988ffd3579e6acbc768c2c06db8b7e08c661c94
Sagar Mandal <Sagar.Mandal@intusurg.com>
2017-07-15 08:18:22 -0700

    Added python script

--- jsonToDB.py
+++ jsonToDB.py
@@ -1,54 +1,59 @@
 #import happybase
 import json
 
-sampleJSON = [{
-		"Company-Name": "Elavon",
-		"Position-Name": "Software Engineer",
-		"Qualifications": ["C", "C++", "Java", "Object Oriented Programming", "C#", "Objective-C"]
-	},
-	{
-		"Company-Name": "HFT Hedge Fund",
-		"Position-Name": "Algorithmic Trading Developer",
-		"Qualifications": ["C", "C++", "Unix", "Low Latency Applications"]
-	},
-	{
-		"Company-Name": "Princeton Consultant",
-		"Position-Name": "Consulting",
-		"Qualifications": ["Data Science", "SciKit Learn"]
-	},
-	{
-		"Company-Name": "Tesla Motors",
-		"Position-Name": "Autopilot Internship/Co-Op",
-		"Qualifications": ["C", "C++", "Computer Vision", "Cuda", "OpenCL", "Python", "Quantitative Analysis"]
-	},
-	{
-		"Company-Name": "Delta Airlines",
-		"Position-Name": "Mechanical Engineer",
-		"Qualifications": ["SQL", "GageRR", "Six Sigma", "AutoCAD", "Inventor", "Solidworks"]
-	},
-	{
-		"Company-Name": "LinkedIn",
-		"Position-Name": "Software Engineering",
-		"Qualifications": ["Python", "Javascript", "Java"]
-	},
-	{
-		"Company-Name": "Intuitive Surgical",
-		"Position-Name": "Clinical Development Engineering",
-		"Qualifications": ["Medical Devices", "Statistics", "Excel"]
-	},
-	{
-		"Company-Name": "Datagne",
-		"Position-Name": "Software Engineering Internship",
-		"Qualifications": ["bachelors, masters", "Object Oriented Programming", "problem solving skills", "team work"]
-	}
-]
 
+sampleJSON =  [
+        {
+    		"Company-Name": "Elavon",
+    		"Position-Name": "Software Engineer",
+    		"Qualifications": ["C", "C++", "Java", "Object Oriented Programming", "C#", "Objective-C"]
+    	},
+    	{
+    		"Company-Name": "HFT Hedge Fund",
+    		"Position-Name": "Algorithmic Trading Developer",
+    		"Qualifications": ["C", "C++", "Unix", "Low Latency Applications"]
+    	},
+    	{
+    		"Company-Name": "Princeton Consultant",
+    		"Position-Name": "Consulting",
+    		"Qualifications": ["Data Science", "SciKit Learn"]
+    	},
+    	{
+    		"Company-Name": "Tesla Motors",
+    		"Position-Name": "Autopilot Internship/Co-Op",
+    		"Qualifications": ["C", "C++", "Computer Vision", "Cuda", "OpenCL", "Python", "Quantitative Analysis"]
+    	},
+    	{
+    		"Company-Name": "Delta Airlines",
+    		"Position-Name": "Mechanical Engineer",
+    		"Qualifications": ["SQL", "GageRR", "Six Sigma", "AutoCAD", "Inventor", "Solidworks"]
+    	},
+    	{
+    		"Company-Name": "LinkedIn",
+    		"Position-Name": "Software Engineering",
+    		"Qualifications": ["Python", "Javascript", "Java"]
+    	},
+    	{
+    		"Company-Name": "Intuitive Surgical",
+    		"Position-Name": "Clinical Development Engineering",
+    		"Qualifications": ["Medical Devices", "Statistics", "Excel"]
+    	}
+    ]
+table.put(b'row-key', {b'cf:col1': b'value1'})
 
 def jobDescriptionToDB(jobDescriptionsJSON):
-    #connection = happybase.Connection('localhost')
-    #connection.open()
+    connection = happybase.Connection('localhost')
+    connection.open()
+    table2 = connection.table('job')
 
-    jobDescriptionDict = json.loads(jobDescriptionJSON)
-    print jobDescriptionDict
+    for jobDict in jobDescriptionsJSON:
+        str key = jobDict["Position-Name"] + '@' + jobDict["Company-Name"]
+        table2.put(key, {'company:name': jobDict["Company-Name"]}
+        counter = 0
+        for qualifications in jobDict['Qualifications']:
+            col_name = 'qual' + str(counter)
+            counter++
+            cf='qualification:'+col_name
+            table2.put(key, {cf:qualifications})
 
 jobDescriptionToDB(sampleJSON)
