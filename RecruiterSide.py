48a73acf51e4c9e202ac2021f6a55456d1d9f487
Sagar Mandal <Sagar.Mandal@intusurg.com>
2017-07-15 07:54:19 -0700

    Added files

--- RecruiterSide.py
+++ RecruiterSide.py
@@ -1,12 +1,29 @@
 import happybase
 
-connection = happybase.Connection('localhost')
-connection.open()
+def findMatches():
+    connection = happybase.Connection('localhost')
+    connection.open()
+    table2 = connection.table('<jobs>')
+    
 
-table = connection.table('resume')
 
-for key, data in table.scan():
-    print key
-    row = table.row(key, columns = [b'skill'])
-    for keys in row:
-        print row[keys]
+
+def findMatchesForQualifiers(jobQualDict):
+    connection = happybase.Connection('localhost')
+    table1 = connection.table('resume')
+    size = len(jobQualDict)
+    candidates = {}
+    for email, data in table.scan():
+        matches = []
+        jobQualDictCopy = jobQualDict
+        row = table.row(key, columns = [b'skill'])
+        for keys in row:
+            currSkill = row[keys]
+            for quals in jobQualDictCopy:
+                if (currSkill == jobQualDictCopy[quals]):
+                    matches.add(currSkill)
+                    jobQualDictCopy.pop(currSkill, None)
+                    break
+        percentage = int(len(matches)/size * 100)
+        candidates[email] = {'percentage' : percentage, 'matches' : matches}
+    return candidates
---
+++ jsonToDB.py
@@ -0,0 +1,54 @@
+#import happybase
+import json
+
+sampleJSON = [{
+		"Company-Name": "Elavon",
+		"Position-Name": "Software Engineer",
+		"Qualifications": ["C", "C++", "Java", "Object Oriented Programming", "C#", "Objective-C"]
+	},
+	{
+		"Company-Name": "HFT Hedge Fund",
+		"Position-Name": "Algorithmic Trading Developer",
+		"Qualifications": ["C", "C++", "Unix", "Low Latency Applications"]
+	},
+	{
+		"Company-Name": "Princeton Consultant",
+		"Position-Name": "Consulting",
+		"Qualifications": ["Data Science", "SciKit Learn"]
+	},
+	{
+		"Company-Name": "Tesla Motors",
+		"Position-Name": "Autopilot Internship/Co-Op",
+		"Qualifications": ["C", "C++", "Computer Vision", "Cuda", "OpenCL", "Python", "Quantitative Analysis"]
+	},
+	{
+		"Company-Name": "Delta Airlines",
+		"Position-Name": "Mechanical Engineer",
+		"Qualifications": ["SQL", "GageRR", "Six Sigma", "AutoCAD", "Inventor", "Solidworks"]
+	},
+	{
+		"Company-Name": "LinkedIn",
+		"Position-Name": "Software Engineering",
+		"Qualifications": ["Python", "Javascript", "Java"]
+	},
+	{
+		"Company-Name": "Intuitive Surgical",
+		"Position-Name": "Clinical Development Engineering",
+		"Qualifications": ["Medical Devices", "Statistics", "Excel"]
+	},
+	{
+		"Company-Name": "Datagne",
+		"Position-Name": "Software Engineering Internship",
+		"Qualifications": ["bachelors, masters", "Object Oriented Programming", "problem solving skills", "team work"]
+	}
+]
+
+
+def jobDescriptionToDB(jobDescriptionsJSON):
+    #connection = happybase.Connection('localhost')
+    #connection.open()
+
+    jobDescriptionDict = json.loads(jobDescriptionJSON)
+    print jobDescriptionDict
+
+jobDescriptionToDB(sampleJSON)
